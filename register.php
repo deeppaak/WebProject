719a3d1dc125b2437d00c2ef9bf49c4cc2f8ca6a
deeppaak <deepaksd021@gmail.com>
2019-09-30 02:37:30 +0530

    Reg Pg Cmp

--- register.php
+++ register.php
@@ -1,3 +1,21 @@
 <?php 
-    echo "Hello world"
+    $conn = new mysqli("localhost:3308", "root", "root", "hotel");
+    
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+
+    $username =  $_POST['username'];
+    $password =  $_POST['password'];
+    $email = $_POST['email'];
+    
+    $sql = "insert into login(username, email, password) values('$username', '$email', '$password')";
+
+    if ($conn->query($sql)) {
+        echo "<script>window.location = 'home.html';</script>";//Reg Success
+    } else {
+        echo "<script>javascript:history.go(-1);</script>";
+    }
+
+    $conn->close();
 ?>
